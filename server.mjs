687922fb0ac09c1207b60e0ff7bae12d72181005
Ludvig Bennbom <117296547+filipacchi@users.noreply.github.com>
2022-12-27 23:23:10 +0100

    reset

--- server.mjs
+++ server.mjs
@@ -1,19 +1,9 @@
 import { createServer } from 'http';
 
-const http =  require("http");
 
-http.createServer((req, res) => {
+createServer((req, res) => {
   res.write('Hello World!');
   res.end();
 }).listen(process.env.PORT);
 
-
-const io = require('socket.io')(http, {cookie: false});
-
-
-io.on('connection', function (socket) {
-  ///console.log('server')
-  //console.log(data)
-  console.log("User connected" + " "+socket.id)
-});
 
