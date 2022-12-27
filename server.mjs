60629fedf106fee012d20955455d8ccbf5377711
Ludvig Bennbom <117296547+filipacchi@users.noreply.github.com>
2022-12-27 23:21:11 +0100

    test

--- server.mjs
+++ server.mjs
@@ -8,5 +8,12 @@ http.createServer((req, res) => {
 }).listen(process.env.PORT);
 
 
+const io = require('socket.io')(http, {cookie: false});
 
+
+io.on('connection', function (socket) {
+  ///console.log('server')
+  //console.log(data)
+  console.log("User connected" + " "+socket.id)
+});
 
