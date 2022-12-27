4e5af0cd3835bd9731f63b007951fc3a654c9221
Ludvig Bennbom <117296547+filipacchi@users.noreply.github.com>
2022-12-27 23:17:16 +0100

    http

--- server.mjs
+++ server.mjs
@@ -1,6 +1,12 @@
 import { createServer } from 'http';
 
-createServer((req, res) => {
+const http =  require("http");
+
+http.createServer((req, res) => {
   res.write('Hello World!');
   res.end();
 }).listen(process.env.PORT);
+
+
+
+
